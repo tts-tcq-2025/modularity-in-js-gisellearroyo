fa8d71d3814375ca9037a7826125d1278c1230b3
US\ARG2GA <arg2ga@bosch.com>
2025-09-10 09:58:31 -0600

    modularity

---
+++ app/colorConstants.js
@@ -0,0 +1,8 @@
+const MajorColorNames = [
+    "WHITE", "RED", "BLACK", "YELLOW", "VIOLET"
+];
+const MinorColorNames = [
+    "BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"
+];
+
+module.exports = { MajorColorNames, MinorColorNames };
